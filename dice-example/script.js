d0a7e04ff268a3a78da34262d125d27132a43ada
Noziphezinhle Nzimande <125486198+Nozzie-Dev@users.noreply.github.com>
2024-06-28 21:36:00 +0200

    adjust layout and add game rules validation

--- dice-example/script.js
+++ dice-example/script.js
@@ -1,57 +1,77 @@
-
 let player1 = "Player1";
 let player2 = "Player2";
+let player1Wins = 0;
+let player1Losses = 0;
+let player1Ties = 0;
+let player2Wins = 0;
+let player2Losses = 0;
+let player2Ties = 0;
 
 function editNames(){
-    player1 = prompt("Change Player1 Name")
-    player2 = prompt("Change Player2 Name")
+    player1 = prompt("Change Player1 Name");
+    player2 = prompt("Change Player2 Name");
 
-    if(player1.length<1 || player2.length<1){
-        alert('please enter valid name');
+    if(player1.length < 1 || player2.length < 1){
+        alert('Please enter valid names');
         return;
     }
-     document.querySelector("p.Player1")
-        .innerHTML = player1;
 
-
-        document.querySelector("p.Player2")
-        .innerHTML = player2;
+    document.querySelector("p.Player1").innerHTML = player1;
+    document.querySelector("p.Player2").innerHTML = player2;
 }
 
-     function rollTheDice(){
-        let diceNum1 = document.querySelector('.img1');
-        let diceNum2 =document.querySelector(".img2");
+function updateScore(){
+   console.log("Displaying scores--");
+   document.querySelector(".winOne").innerHTML = `Wins: ${player1Wins}`;
+   document.querySelector(".lossOne").innerHTML = `Loses: ${player1Losses}`;
+   document.querySelector(".tieOne").innerHTML = `Ties: ${player1Ties}`;
+   document.querySelector(".winTwo").innerHTML = `Wins: ${player2Wins}`;
+   document.querySelector(".lossTwo").innerHTML = `Loses: ${player2Losses}`;
+   document.querySelector(".tieTwo").innerHTML = `Ties: ${player2Ties}`;
+   console.log("Score set");
+}
 
-        diceNum1.setAttribute("src","images/dice-gif.gif")
-        diceNum2.setAttribute("src","images/dice-gif.gif")
+function rollTheDice(){
+    let diceNum1 = document.querySelector('.img1');
+    let diceNum2 = document.querySelector(".img2");
 
-        let result = document.querySelector('h1')
-        setTimeout(() => {
-         let randomNumber1 = Math.floor(Math.random()*6)+1;
-         let randomNumber2 = Math.floor(Math.random()*6)+1;
+    diceNum1.setAttribute("src", "images/dice-gif.gif");
+    diceNum2.setAttribute("src", "images/dice-gif.gif");
 
+    let result = document.querySelector('h1');
+    setTimeout(() => {
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-         diceNum1.setAttribute('src','images/dice-'+randomNumber1+'.jpg');
-         diceNum2.setAttribute('src','images/dice-'+randomNumber2+'.jpg');
+        diceNum1.setAttribute('src', 'images/dice-' + randomNumber1 + '.jpg');
+        diceNum2.setAttribute('src', 'images/dice-' + randomNumber2 + '.jpg');
 
-     let score = document.querySelectorAll('.score')
-     score.forEach()//not done
+        if (randomNumber1 === randomNumber2) {
+            result.innerHTML = "Draw!";
+            player1Ties++;
+            player2Ties++;
+        } else if (randomNumber1 < randomNumber2) {
+            result.innerHTML = (player2 + " WINS!");
+            player2Wins++;
+            player1Losses++;
+        } else {
+            result.innerHTML = (player1 + " WINS!");
+            player1Wins++;
+            player2Losses++;
+        }
+        updateScore();
 
-     //determine the winner//
-     if(randomNumber1=== randomNumber2){
-        result.innerHTML = "Draw!"
-        player1 && player1 
-     }
-     else if(randomNumber1<randomNumber2){
-        result.innerHTML = (player2 +" WINS!");
-     }
-     else{
-        result.innerHTML = (player1 +" WINS!");
-     }
+    }, 2500);
+}
 
+function newGame(){
+    player1Wins = 0;
+    player1Losses = 0;
+    player1Ties = 0;
+    player2Wins = 0;
+    player2Losses = 0;
+    player2Ties = 0;
 
-
-        },2500);
-     }
-
-    
+    updateScore();
+    document.querySelector('h1').innerHTML = "Play!";
+}
